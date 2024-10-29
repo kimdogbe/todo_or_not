861fe2e53424ea6102a0ddcc1b265101145ae573
kimdogbe <kimdogbe@gmail.com>
2024-10-29 11:12:07 +0000

    Add dialog for new item and extend functionality
    The dialog for adding an item to a list has now been added.
    Alongside this I have also added some additional functionality
    to get the data from the dialog and into the data object
    that contains the list data.

--- src/index.js
+++ src/index.js
@@ -5,34 +5,55 @@ import { updateCards } from "./domUpdates.js";
 console.log("We out here");
 
 let allLists = [];
-const dialogElement = document.querySelector("dialog");
-const formElement = document.querySelector("form");
+const newListDialog = document.querySelector("#new-list-dialog");
+const addItemDialog = document.querySelector("#add-item-dialog");
+const newListForm = document.querySelector("#new-list-form");
+const addItemForm = document.querySelector("#add-item-form");
 const listName = document.querySelector("#list-name");
 const listDesc = document.querySelector("#list-desc");
 
 addEventListener("click", (event) => {
   if(event.target.id == "new-list-btn") {
-    dialogElement.showModal();
+    newListDialog.showModal();
   }
   else if(event.target.id == "cancel-list") {
     event.preventDefault();
-    dialogElement.close();
+    newListDialog.close();
+  }
+  else if(event.target.id == "cancel-item") {
+    event.preventDefault();
+    addItemDialog.close();
   }
   else if (event.target.classList[0] == "add-item-btn") {
     // TODO: add item to list
+    addItemDialog.showModal();
     const listIndex = event.target.classList[1].split("-")[1];
     const targetList = allLists[listIndex];
-    // targetList.createNewItem();
+
+    addItemForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const itemTitle = document.querySelector("#item-title");
+      const itemDesc = document.querySelector("#item-desc");
+      const itemPriority = document.querySelector("#item-priority");
+      const itemDueDate = document.querySelector("#item-duedate");
+      targetList.createNewItem(itemTitle.value, itemDesc.value, itemDueDate.value, itemPriority.value);
+      console.log(allLists);
+      updateCards(allLists);
+      console.log(targetList.getIncompleteItems());
+      addItemDialog.close();
+    });
 
     console.log(listIndex);
   }
 });
 
-formElement.addEventListener("submit", (event) => {
+newListForm.addEventListener("submit", (event) => {
     event.preventDefault();
     allLists.push(createList(listName.value, listDesc.value));
     console.log(allLists);
     updateCards(allLists);
-    dialogElement.close();
+    newListDialog.close();
 });
+
+
 
